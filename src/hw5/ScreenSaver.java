7138ea8910f09696cfc067dd8a25811d9a508c2f
Russ Forstall <russ4stall@gmail.com>
2015-06-24 19:43:09 -0400

    finished hw5

--- src/hw5/ScreenSaver.java
+++ src/hw5/ScreenSaver.java
@@ -11,21 +11,20 @@ import java.util.Random;
  * @author Russ Forstall
  */
 public class ScreenSaver {
-    // execute application
     public static void main( String args[] ) {
-        JFrame frame = new JFrame( "Screen Saver" );
+        JFrame frame = new JFrame("Screen Saver");
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
         LineJPanel panel = new LineJPanel();
-        frame.add( panel );
-        frame.setSize( 300, 300 ); // set frame size
-        frame.setVisible( true ); // display frame
+        frame.add(panel);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
     }
 }
 
 class LineJPanel extends JPanel implements ActionListener
 {
-    private int delay = 10;
+    private int delay = 1000;
     protected Timer timer;
 
     public LineJPanel() {
@@ -37,18 +36,20 @@ class LineJPanel extends JPanel implements ActionListener
         repaint();
     }
 
-    // draw rectangles and arcs
-    public void paintComponent( Graphics g ) {
-        super.paintComponent( g ); // call superclass's paintComponent
-        Graphics2D g2d = ( Graphics2D ) g;
-        g2d.setColor(Color.red);
-        g2d.setStroke(new BasicStroke(10));
-
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
         Random random = new Random();
 
-        Line2D.Double line = new Line2D.Double(20, 22, 40, 42);
-        g2d.draw(line);
+        for (int i = 0; i < 100; i++){
+            g2d.setColor(new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+            g2d.setStroke(new BasicStroke(random.nextInt(20) + 1));
+            int x1 = random.nextInt(getWidth());
+            int x2 = random.nextInt(getWidth());
+            int y1 = random.nextInt(getHeight());
+            int y2 = random.nextInt(getHeight());
+            Line2D.Double line = new Line2D.Double(x1, y1, x2, y2);
+            g2d.draw(line);
+        }
     }
-
-
 }
