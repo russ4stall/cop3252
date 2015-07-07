c9c973cb9e5b7c4732dd63700d9afe29cee9ccaa
Russ Forstall <russ4stall@gmail.com>
2015-07-07 01:30:04 -0400

    hw6 part2

--- src/hw6/Guess.java
+++ src/hw6/Guess.java
@@ -1,7 +1,5 @@
 package hw6;
 
-import hw2.IntegerSet;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,65 +8,105 @@ import java.util.Random;
 
 public class Guess {
     private int number;
+    private int previousGuess;
     private JFrame frame;
     private JLabel statusLabel;
+    private JLabel guessLabel;
     private JPanel panel;
+    private JTextField userGuessField;
 
-    public Guess(){
+    public Guess() {
         init();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Guess guess = new Guess();
         guess.guess();
     }
 
-    private void init(){
+    private void init() {
         number = new Random().nextInt(999) + 1;
 
         frame = new JFrame("Guess");
-        frame.setSize(400,400);
+        frame.setSize(700, 100);
         frame.setLayout(new FlowLayout());
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
-        statusLabel = new JLabel("",JLabel.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        statusLabel = new JLabel("", JLabel.CENTER);
 
-        statusLabel.setSize(350,100);
+        statusLabel.setSize(350, 100);
 
         panel = new JPanel();
         panel.setLayout(new FlowLayout());
 
         frame.add(panel);
-        frame.add(statusLabel);
+        //frame.add(statusLabel);
         frame.setVisible(true);
     }
 
-    private void guess(){
+    private void guess() {
 
-        JLabel guessLabel= new JLabel("Guess: ", JLabel.RIGHT);
-        final JTextField userGuessField = new JTextField(4);
+        guessLabel = new JLabel("I have a number between 1 and 1000. Can you guess my number?");
+        userGuessField = new JTextField(4);
 
         userGuessField.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String data = "Guess: " + userGuessField.getText();
-                int guess = Integer.parseInt( userGuessField.getText());
-                int guessResult = guessAction(guess);
-                if (  guessResult > 0 ) {
-                    // warmer
-                } else if( )
-
-                statusLabel.setText(data);
-                frame.setBackground(Color.RED);
+                int guess = Integer.parseInt(userGuessField.getText());
+                guessAction(guess);
             }
         });
 
         panel.add(guessLabel);
         panel.add(userGuessField);
+        panel.add(statusLabel);
         frame.setVisible(true);
     }
 
-    private int guessAction(int guess) {
+    private void guessAction(int guess) {
+        int currentDiff = Math.abs(guess - number);
+        int previousDiff = Math.abs(previousGuess - number);
 
+        if (guess == number) {
+            statusLabel.setText("CORRECT!");
+            winAction();
+        } else {
+            if (currentDiff > previousDiff) {
+                frame.setBackground(Color.RED);
+                panel.setBackground(Color.RED);
+            } else if (currentDiff < previousDiff) {
+                frame.setBackground(Color.BLUE);
+                panel.setBackground(Color.BLUE);
+            } else {
+                frame.setBackground(Color.WHITE);
+                panel.setBackground(Color.WHITE);
+            }
 
-        return 1;
+            if (guess < number) {
+                statusLabel.setText("Too Low");
+            } else {
+                statusLabel.setText("Too High");
+            }
+        }
+
+        previousGuess = guess;
+    }
+
+    private void winAction() {
+        userGuessField.setEditable(false);
+        final JButton playAgainButton = new JButton("Play Again");
+        guessLabel.setText("");
+        playAgainButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                userGuessField.setEditable(true);
+                frame.setBackground(Color.WHITE);
+                panel.setBackground(Color.WHITE);
+                number = new Random().nextInt(999) + 1;
+                panel.remove(playAgainButton);
+                statusLabel.setText("");
+                guessLabel.setText("I have a number between 1 and 1000. Can you guess my number?");
+            }
+        });
+        panel.add(playAgainButton);
     }
 }
---
+++ src/hw6_2/Guess.java
@@ -0,0 +1,21 @@
+package hw6_2;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Russ Forstall
+ */
+public class Guess {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class GuessPanel extends JPanel {
+    public void paint(Graphics g) {
+
+    }
+}
+
